d0aad5a698914f6fcb2cea584195dbd0897e7e6d
kanishka1312 <kanishkaagarwal1312@gmail.com>
2023-01-14 18:46:46 +0530

    Contact Page Updated

---
+++ src/component/ContactPage/ContactBody.js
@@ -0,0 +1,17 @@
+export default function ContactBody(){
+    return(
+        <>
+        <div>
+            <div className="bg-white p-52">
+                <div className="flex space-x-20">
+                    <h2 className="flex-1 text-4xl font-medium text-blue-400">
+                    Send a Message
+                    </h2><br/>
+                    
+                    
+                    </div>
+            </div>
+        </div>
+        </>
+    )
+}
---
+++ src/component/ContactPage/ContactHeader.js
@@ -0,0 +1,13 @@
+export default function ContactHeader(){
+    return(
+        <>
+        <div className=" text-white">
+            <div className="flex justify-center py-96">
+                <div className="">
+                <h1 className=" text-7xl  flex justify-center font-bold py-2">Contact Us</h1>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
